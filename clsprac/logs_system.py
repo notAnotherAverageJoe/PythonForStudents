b814c2891b51e0048d44bbc72bc4cd4b969964de
joseph skokan <joeskokan20@gmail.com>
2025-05-01 01:53:16 -0400

    more cls

---
+++ clsprac/logs_system.py
@@ -0,0 +1,38 @@
+
+
+
+
+class Logger:
+    def login(self, message):
+        print(f"Login: {message}")
+    def logout(self, message):
+        print(f"Logout: {message}")
+        
+logger = Logger()
+logger.login("Hello")
+logger.logout("Goodbye!")
+
+class System:
+    def __init__(self, mode):
+        self.mode = mode
+        self.logs = Logger()
+    def power_on(self):
+        if self.mode == 1:
+            print("Powering on")
+            self.logs.login("Welcome back!")
+        elif self.mode == 2:
+            print("Power saving mode")
+        elif self.mode == 3:
+            print("Power off")
+        else:
+            print("Invalid system operation")
+computer1 = System(1)
+computer1.power_on()
+
+            
+        
+
+
+
+
+
--- temps.py
+++ temps.py
@@ -1,13 +1,38 @@
-class Duck:
-    def quack(self):
-        print("Quack!")
 
-class Person:
-    def quack(self):
-        print("I'm pretending to be a duck!")
 
-def make_it_quack(thing):
-    thing.quack()  # It doesn't matter *what* thing is, as long as it has a quack() method
 
-make_it_quack(Duck())     # ✅ Quack!
-make_it_quack(Person())   # ✅ I'm pretending to be a duck!
+
+class Logger:
+    def login(self, message):
+        print(f"Login: {message}")
+    def logout(self, message):
+        print(f"Logout: {message}")
+        
+# logger = Logger()
+# logger.login("Hello")
+# logger.logout("Goodbye!")
+
+# class System:
+#     def __init__(self, mode):
+#         self.mode = mode
+#         self.logs = Logger()
+#     def power_on(self):
+#         if self.mode == 1:
+#             print("Powering on")
+#             self.logs.login("Welcome back!")
+#         elif self.mode == 2:
+#             print("Power saving mode")
+#         elif self.mode == 3:
+#             print("Power off")
+#         else:
+#             print("Invalid system operation")
+# computer1 = System(1)
+# computer1.power_on()
+
+            
+        
+
+
+
+
+
